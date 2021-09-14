923a44b1e23999ae7f10ac020a4aa839a3a67eff
LroseC <1441276292@qq.com>
2021-09-14 21:08:39 +0800

    finish chess game

---
+++ Chess/main.cpp
@@ -0,0 +1,138 @@
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+// #define getch() (p1 == p2 && (p2 = (p1 = buf) + fread(buf, 1, 1 << 20, stdin), p1 == p2) ? EOF : *p1++)
+#define getch() getchar()
+char buf[1 << 20], *p1, *p2;
+
+inline bool iswasd(char ch)
+{
+	if (ch == 'w' || ch == 'W') return 1;
+	if (ch == 'a' || ch == 'A') return 1;
+	if (ch == 's' || ch == 'S') return 1;
+	if (ch == 'd' || ch == 'D') return 1;
+	return 0;
+}
+
+class ChessGame
+{
+	public:
+		int main(void) {
+			memset(mp, 0, sizeof mp);
+			while (!full() && !win) {
+				Print();
+				char ch = getch();
+				if (iswasd(ch)) MoveCursor(ch);
+				else if (ch == 'y') Change();
+			}
+			Print();
+			if (win == 0) puts("NIE");
+			if (win == 1) puts("X win!");
+			if (win == 2) puts("O win!");
+			return 0;
+		}
+	private:
+		int turn = 0;
+		int x = 1, y = 1;
+		int win = 0;
+		char mp[3][3];
+		bool full(void)
+		{
+			for (int i = 0; i < 3; ++i)
+				for (int j = 0; j < 3; ++j)
+					if (!mp[i][j])
+						return 0;
+			return 1;
+		}
+		int Check(void)
+		{
+			for (int i = 0; i < 3; ++i) {
+				bool seccess = 1;
+				if (mp[i][0] == 0) continue;
+				for (int j = 1; j < 3; ++j) {
+					if (mp[i][j] != mp[i][0])
+						seccess = 0;
+				}
+				if (seccess) return mp[i][0] == 'X' ? 1 : 2;
+			}
+			for (int j = 0; j < 3; ++j) {
+				bool seccess = 1;
+				if (mp[0][j] == 0) continue;
+				for (int i = 1; i < 3; ++i)
+					if (mp[i][j] != mp[0][j])
+					seccess = 0;
+				if (seccess) return mp[0][j] == 'X' ? 1 : 2;
+			}
+			bool seccess = 1;
+			if (mp[0][0] == 0) seccess = 0;
+			for (int i = 1; i < 3; ++i)
+				if (mp[i][i] != mp[0][0])
+					seccess = 0;
+			if (seccess) return mp[0][0] == 'X' ? 1 : 2;
+			seccess = 1;
+			if (mp[0][2] == 0) seccess = 0;
+			for (int i = 1; i < 3; ++i)
+				if (mp[i][2 - i] != mp[0][2])
+					seccess = 0;
+			if (seccess) return mp[0][2] == 'X' ? 1 : 2;
+			return 0;
+		}
+		void Print(void)
+		{
+			system("clear");
+			puts("+-+-+-+");
+			for (int i = 0; i < 3; ++i) {
+				putchar('|');
+				for (int j = 0; j < 3; ++j) {
+					if (i == x && j == y) {
+						if (mp[i][j] == 'O') printf("o|");
+						else if (mp[i][j] == 'X') printf("x|");
+						else printf("n|");
+					}
+					else printf("%c|", (mp[i][j] ? mp[i][j] : 'N'));
+				}
+				puts("");
+				puts("+-+-+-+");
+			}
+		}
+		void MoveCursor(char ch) {
+			if (ch == 'w' || ch == 'W') {
+				x = x + 2;
+				x = x > 2 ? x - 3 : x;
+			}
+			if (ch == 'a' || ch == 'A') {
+				y = y + 2;
+				y = y > 2 ? y - 3 : y;
+			}
+			if (ch == 's' || ch == 'S') {
+				x = x + 1;
+				x = x > 2 ? x - 3 : x;
+			}
+			if (ch == 'd' || ch == 'D') {
+				y = y + 1;
+				y = y > 2 ? y - 3 : y;
+			}
+		}
+		void Change()
+		{
+			if (mp[x][y]) return;
+			mp[x][y] = (turn & 1) ? 'O' : 'X';
+			++turn;
+			win = Check();
+		}
+}game;
+
+int main(void)
+{
+	puts("use WASD to move the cursor");
+	puts("use y to determind");
+	puts("input b to begin");
+	char ch;
+	while (ch = getchar() != 'b');
+	game.main();
+	return 0;
+}
